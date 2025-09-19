a7ff2596ba01578066bef7728589c8a2ca4928ba
bricksky <skydh1214@naver.com>
2025-09-20 00:30:29 +0900

    chore: Spring Boot 프로젝트 초기 설정 (Java 17, Gradle, 기본 구조 추가)

---
+++ src/main/java/com/example/moabackend/MoABackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.moabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoABackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoABackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moabackend/MoABackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoABackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
