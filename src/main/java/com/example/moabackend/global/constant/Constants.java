1d3c5caa7d75dedaad6a3ae286f48c83cb6b6bd2
SSUN <rlatjsghks4647@naver.com>
2025-09-30 18:35:15 +0900

    feat : Constants 추가

---
+++ src/main/java/com/example/moabackend/global/constant/Constants.java
@@ -0,0 +1,24 @@
+package com.example.moabackend.global.constant;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Constants {
+    public static String CLAIM_USER_ID = "uuid";
+    public static String CLAIM_USER_ROLE = "role";
+    public static String REFRESH_COOKIE_NAME = "refresh_token";
+
+    public static List<String> NO_NEED_AUTH = List.of(
+            "/api/v1/health-check",
+            "/swagger",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/api-docs",
+            "/api-docs/**",
+            "/v3/api-docs/**",
+            "/api/auth/reissue",
+            "/ws/**",
+            "/api/auth/logout"
+    );
+    public static final Duration SUBSCRIPTION_TTL = Duration.ofHours(24);
+}
