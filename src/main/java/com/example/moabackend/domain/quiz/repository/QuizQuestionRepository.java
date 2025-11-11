381805466cce4bd3b8f3119eb48f76144d3760b2
bricksky <skydh1214@naver.com>
2025-11-11 15:01:49 +0900

    refactor: 퀴즈 리포지토리 및 서비스 로직 개선 - QuizQuestionRepository에서 문자열 타입을 EQuizType으로 변경 - QuizResultRepository에 비관적 잠금 처리 추가 - QuizResultRepository의 무작위 퀴즈 조회 쿼리 파라미터 정리 - QuizResultServiceImpl 변수명 및 트랜잭션 옵션 수정 - QuizQuestionServiceImpl에서 쿼리 호출 시 EQuizType 활용하도록 수정

--- src/main/java/com/example/moabackend/domain/quiz/repository/QuizQuestionRepository.java
+++ src/main/java/com/example/moabackend/domain/quiz/repository/QuizQuestionRepository.java
@@ -1,18 +1,21 @@
 package com.example.moabackend.domain.quiz.repository;
 
 import com.example.moabackend.domain.quiz.entity.QuizQuestion;
+import com.example.moabackend.domain.quiz.entity.type.EQuizType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+
 public interface QuizQuestionRepository extends JpaRepository<QuizQuestion, Long> {
+
     @Query(value = """
                 SELECT * FROM quiz_question q
                 WHERE q.type = :type
                 ORDER BY RAND()
                 LIMIT :count
             """, nativeQuery = true)
-    List<QuizQuestion> findRandomNByType(@Param("type") String type, @Param("count") int count);
+    List<QuizQuestion> findRandomNByType(@Param("type") EQuizType type, @Param("count") int count);
 }
--- src/main/java/com/example/moabackend/domain/quiz/repository/QuizResultRepository.java
+++ src/main/java/com/example/moabackend/domain/quiz/repository/QuizResultRepository.java
@@ -2,7 +2,10 @@ package com.example.moabackend.domain.quiz.repository;
 
 import com.example.moabackend.domain.quiz.entity.QuizResult;
 import com.example.moabackend.domain.quiz.entity.type.EQuizType;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
@@ -10,13 +13,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface QuizResultRepository extends JpaRepository<QuizResult, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<QuizResult> findByUserIdAndDateAndType(Long userId, LocalDate date, EQuizType quizType);
+
     @Query("""
                 SELECT DISTINCT q.type
                 FROM QuizResult q
                 WHERE q.user.id = :userId
                 AND DATE(q.date) = :date
             """)
-    List<EQuizType> findCompletedQuizTypes(Long userId, LocalDate date);
-
-    Optional<QuizResult> findByUserIdAndDateAndType(Long userId, LocalDate date, EQuizType quizType);
+    List<EQuizType> findCompletedQuizTypes(@Param("userId") Long userId, @Param("date") LocalDate date);
 }
