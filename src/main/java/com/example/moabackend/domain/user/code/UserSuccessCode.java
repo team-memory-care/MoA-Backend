5417a8790832e9e24f89367db8e976f14c4e513c
SSUN <rlatjsghks4647@naver.com>
2025-11-13 22:08:10 +0900

    feat : UserSuccessCode 추가

--- src/main/java/com/example/moabackend/domain/user/code/UserSuccessCode.java
+++ src/main/java/com/example/moabackend/domain/user/code/UserSuccessCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserSuccessCode implements SuccessCode {
     USER_REGISTER_TEMP_SAVED(HttpStatus.ACCEPTED, "회원가입 기본 정보가 임시 저장되었습니다."),
-    AUTH_CODE_SENT(HttpStatus.OK, "인증코드가 발송되었습니다. 유효시간 5분.");
+    AUTH_CODE_SENT(HttpStatus.OK, "인증코드가 발송되었습니다. 유효시간 5분."),
+    REISSUE_SUCCESS(HttpStatus.OK, "토큰을 재발급하였습니다."),
+    LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃에 성공하였습니다.")
     ;
 
     private final HttpStatus httpStatus;
