48d02c8574c9e61f0f36d91914c158bb4062551e
bricksky <skydh1214@naver.com>
2025-11-15 20:49:43 +0900

    feat: AWS S3 설정 추가 - application-prod.yml에 AWS S3 설정값 추가 - S3Config 클래스 작성 및 S3Client 빈 등록 - 빌드 파일에 AWS S3 라이브러리 의존성 추가 - 파일 업로드를 위한 multipart 설정 추가

---
+++ src/main/java/com/example/moabackend/global/config/S3Config.java
@@ -0,0 +1,31 @@
+package com.example.moabackend.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.access-key}")
+    private String accessKey;
+
+    @Value("${aws.s3.secret-key}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client(){
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .region(Region.of(region))
+                .build();
+    }
+}
