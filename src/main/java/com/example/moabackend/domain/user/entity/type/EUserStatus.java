9943049bb94b6a5ea8f005b1367d80323759cc51
SSUN <rlatjsghks4647@naver.com>
2025-11-20 02:17:57 +0900

    feat : EUserStatus에 WITHDRAWN 추가

--- src/main/java/com/example/moabackend/domain/user/entity/type/EUserStatus.java
+++ src/main/java/com/example/moabackend/domain/user/entity/type/EUserStatus.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum EUserStatus {
 
     ACTIVE("active"),
-    INACTIVE("inactive");
+    INACTIVE("inactive"),
+    WITHDRAWN("withdrawn");
 
     private final String value;
 }
