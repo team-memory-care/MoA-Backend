f9dadd596d6d17772ab2e4a377c5ff8881d55ee5
SSUN <rlatjsghks4647@naver.com>
2025-11-20 10:13:31 +0900

    feat: reissue 시 refreshtoken, role 반환

--- src/main/java/com/example/moabackend/global/token/dto/res/ReissueTokenResponseDto.java
+++ src/main/java/com/example/moabackend/global/token/dto/res/ReissueTokenResponseDto.java
@@ -1,9 +1,12 @@
 package com.example.moabackend.global.token.dto.res;
 
+import com.example.moabackend.domain.user.entity.type.ERole;
+
 public record ReissueTokenResponseDto(
-        String accessToken
-) {
-    public static ReissueTokenResponseDto from(String accessToken){
-        return new ReissueTokenResponseDto(accessToken);
+        String accessToken,
+        String refreshToken,
+        ERole role) {
+    public static ReissueTokenResponseDto from(String accessToken, String refreshToken, ERole role) {
+        return new ReissueTokenResponseDto(accessToken, refreshToken, role);
     }
 }
