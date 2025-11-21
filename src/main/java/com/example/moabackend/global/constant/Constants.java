109d7f92ed46af5ef09d778c4c55bd266620795d
bricksky <skydh1214@naver.com>
2025-11-21 13:19:22 +0900

    feat: 인증 및 퀴즈 API 경로 업데이트 - 새로운 인증 및 퀴즈 API 경로 추가 - 사용하지 않는 경로 주석 처리 및 정리 - SecurityConfig에서 퀴즈 및 역할 관련 경로 권한 설정 수정

--- src/main/java/com/example/moabackend/global/constant/Constants.java
+++ src/main/java/com/example/moabackend/global/constant/Constants.java
@@ -16,19 +16,24 @@ public class Constants {
             "/api-docs",
             "/api-docs/**",
             "/v3/api-docs/**",
-            "/api/v1/auth/reissue",
-            "/api/v1/auth/logout",
+
             "/api/v1/users/signup",
-            "/api/v1/users/signup-confirm",
-            "/api/v1/users/login",
-            "/api/v1/users/register",
-            "/api/v1/users/register/code-request",
-            "/api/v1/users/register/code-complete",
-            "/api/v1/auth/send-code",
-            "/api/v1/auth/verify-code",
+            "/api/v1/users/signup/sms",
+            "/api/v1/users/role/parent",
+            //"/api/v1/users/role/child",
+            "/api/v1/users/parent-code",
+            "/api/v1/users/withdraw",
+
+            "/api/v1/auth/token/reissue",
             "/api/v1/auth/sms/request",
+            "/api/v1/auth/logout",
             "/api/v1/auth/login",
-            "/api/v1/auth/code/issue",
+
+            "/api/v1/quiz/submit",
+            "/api/v1/quiz/result",
+            "/api/v1/quiz/today",
+            "/api/v1/quiz/set",
+            "/api/v1/quiz/remain",
             "/api/v1/s3/upload"
     );
 }
