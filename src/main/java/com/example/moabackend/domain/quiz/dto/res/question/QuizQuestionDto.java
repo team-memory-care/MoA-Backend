dd8914a90e6e79c769c29fba88c6ecb7240dd5b4
SSUN <96679633+SeonHwan-Kim@users.noreply.github.com>
2025-11-24 01:21:28 +0900

    feat : DTO에 answer 필드 추가 (#56)

--- src/main/java/com/example/moabackend/domain/quiz/dto/res/question/QuizQuestionDto.java
+++ src/main/java/com/example/moabackend/domain/quiz/dto/res/question/QuizQuestionDto.java
@@ -4,27 +4,25 @@ import com.example.moabackend.domain.quiz.entity.type.EQuizType;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.EXISTING_PROPERTY,
-        property = "quizType"
-)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "quizType")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = PersistenceQuizQuestionDto.class, name = "PERSISTENCE"),
-        @JsonSubTypes.Type(value = MemoryQuizQuestionDto.class, name = "MEMORY"),
-        @JsonSubTypes.Type(value = LinguisticQuizQuestionDto.class, name = "LINGUISTIC"),
-        @JsonSubTypes.Type(value = AttentionQuizQuestionDto.class, name = "ATTENTION"),
-        @JsonSubTypes.Type(value = SpacetimeQuizQuestionDto.class, name = "SPACETIME")
+                @JsonSubTypes.Type(value = PersistenceQuizQuestionDto.class, name = "PERSISTENCE"),
+                @JsonSubTypes.Type(value = MemoryQuizQuestionDto.class, name = "MEMORY"),
+                @JsonSubTypes.Type(value = LinguisticQuizQuestionDto.class, name = "LINGUISTIC"),
+                @JsonSubTypes.Type(value = AttentionQuizQuestionDto.class, name = "ATTENTION"),
+                @JsonSubTypes.Type(value = SpacetimeQuizQuestionDto.class, name = "SPACETIME")
 })
 
 public sealed interface QuizQuestionDto permits
-        PersistenceQuizQuestionDto, LinguisticQuizQuestionDto, MemoryQuizQuestionDto
-        , AttentionQuizQuestionDto, SpacetimeQuizQuestionDto {
-    Long questionId();
+                PersistenceQuizQuestionDto, LinguisticQuizQuestionDto, MemoryQuizQuestionDto, AttentionQuizQuestionDto,
+                SpacetimeQuizQuestionDto {
+        Long questionId();
 
-    EQuizType quizType();
+        EQuizType quizType();
 
-    String questionFormat();
+        String questionFormat();
 
-    String questionContent();
+        String questionContent();
+
+        String answer();
 }
