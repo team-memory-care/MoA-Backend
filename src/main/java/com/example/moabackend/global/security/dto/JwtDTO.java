cf93e578a32220c8d6b215da5a0a8c709f622c26
bricksky <skydh1214@naver.com>
2025-11-26 14:02:37 +0900

    feat: JWT 토큰 생성 시 역할 포함 - `JwtDTO`에 역할(ERole) 필드 추가 - `generateTokens` 메서드에서 역할 정보 포함 토큰 생성 - 역할 필드 추가에 따른 `JwtDTO.of` 정적 메서드 수정 - 사용하지 않는 `Constants` import 제거

--- src/main/java/com/example/moabackend/global/security/dto/JwtDTO.java
+++ src/main/java/com/example/moabackend/global/security/dto/JwtDTO.java
@@ -1,16 +1,19 @@
 package com.example.moabackend.global.security.dto;
 
+import com.example.moabackend.domain.user.entity.type.ERole;
 import lombok.Builder;
 
 @Builder
 public record JwtDTO(
         String accessToken,
-        String refreshToken
+        String refreshToken,
+        ERole role
 ) {
-    public static JwtDTO of(String accessToken, String refreshToken){
+    public static JwtDTO of(String accessToken, String refreshToken, ERole role){
         return JwtDTO.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
+                .role(role)
                 .build();
     }
 }
