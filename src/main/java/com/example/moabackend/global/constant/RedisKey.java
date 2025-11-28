bdd4b636369c048864e8c9bc357ac61123d39c16
SSUN <rlatjsghks4647@naver.com>
2025-11-29 01:21:29 +0900

    feat : Redis Stream Key 추가

--- src/main/java/com/example/moabackend/global/constant/RedisKey.java
+++ src/main/java/com/example/moabackend/global/constant/RedisKey.java
@@ -1,5 +1,13 @@
 package com.example.moabackend.global.constant;
 
 public class RedisKey {
-    public static String REFRESH_TOKEN_PREFIX = "user:refresh_token:";
+    public static final String REFRESH_TOKEN_PREFIX = "user:refresh_token:";
+
+    // redis stream key
+    public static final String REPORT_STREAM_KEY = "report-stream";
+    public static final String REPORT_GROUP = "report-group";
+    public static final String REPORT_CONSUMER = "report-worker";
+    public static final String REPORT_MESSAGE_MAP_KEY = "report-payload";
+    public static final String REPORT_RETRY_COUNT = "report-retry-count";
+    public static final String REPORT_DLQ_STREAM_KEY = "report-dlq-stream";
 }
