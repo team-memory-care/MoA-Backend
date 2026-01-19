f8fc64468023cc841f3655d8ee64ce7c993b81dd
SSUN <96679633+SeonHwan-Kim@users.noreply.github.com>
2026-01-20 02:32:37 +0900

    feat : quiz 결과 저장 시 date 추가 (#159)

--- src/main/java/com/example/moabackend/domain/quiz/dto/req/QuizSaveRequestDto.java
+++ src/main/java/com/example/moabackend/domain/quiz/dto/req/QuizSaveRequestDto.java
@@ -3,10 +3,13 @@ package com.example.moabackend.domain.quiz.dto.req;
 import com.example.moabackend.domain.quiz.entity.type.EQuizCategory;
 import com.example.moabackend.domain.quiz.entity.type.EQuizType;
 
+import java.time.LocalDate;
+
 public record QuizSaveRequestDto(
         int totalNumber,
         int correctNumber,
         EQuizType type,
-        EQuizCategory category
+        EQuizCategory category,
+        LocalDate date
 ) {
 }
