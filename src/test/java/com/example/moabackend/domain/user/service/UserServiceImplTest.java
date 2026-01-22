f1b0d05746360cdc4f29d0217877fe181c1e1b7a
bricksky <skydh1214@naver.com>
2026-01-22 12:05:45 +0900

    test: 회원가입 SMS 요청 예외 처리 테스트 추가 - 이미 활성화된 사용자에 대한 예외 발생 케이스 작성 - UserErrorCode와 CustomException을 활용한 검증 로직 추가 - UserRepository의 Mock 동작 정의 및 값 설정

--- src/test/java/com/example/moabackend/domain/user/service/UserServiceImplTest.java
+++ src/test/java/com/example/moabackend/domain/user/service/UserServiceImplTest.java
@@ -1,6 +1,10 @@
 package com.example.moabackend.domain.user.service;
 
+import com.example.moabackend.domain.user.code.UserErrorCode;
+import com.example.moabackend.domain.user.entity.User;
+import com.example.moabackend.domain.user.entity.type.EUserStatus;
 import com.example.moabackend.domain.user.repository.UserRepository;
+import com.example.moabackend.global.exception.CustomException;
 import com.example.moabackend.global.token.service.AuthService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -43,5 +48,25 @@ public class UserServiceImplTest {
 
         // then: 결과 검증
         assertThat(result).isEqualTo(expectedAuthCode);
+    }
+
+    @Test
+    @DisplayName("회원가입 SMS 요청 - 이미 활성화된 유저일 때 에러 발생")
+    void requestSignUpSms_AlreadyExists_ThrowsException() {
+        // given: 상황 준비
+        String phoneNumber = "01035477120";
+        String resolvedNumber = "821035477120";
+
+        User existingUser = User.builder()
+                .phoneNumber(resolvedNumber)
+                .status(EUserStatus.ACTIVE)
+                .build();
+
+        given(userRepository.findByPhoneNumber(resolvedNumber)).willReturn(Optional.of(existingUser));
+
+        // when: 로직 실행 & then: 결과 검증
+        assertThatThrownBy(() -> userService.requestSignUpSms(phoneNumber))
+                .isInstanceOf(CustomException.class)
+                .hasFieldOrPropertyWithValue("errorCode", UserErrorCode.USER_ALREADY_EXISTS);
     }
 }
